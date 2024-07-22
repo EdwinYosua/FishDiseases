fbb11df3280da1cee68afb32bc06037f00bf9faa
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-07-22 17:13:02 +0800

    Component Button
    fixed button color when pressed

--- app/src/main/java/com/edwinyosua/fishdiseasesapp/ui/component/CustomButtonOnBoarding.kt
+++ app/src/main/java/com/edwinyosua/fishdiseasesapp/ui/component/CustomButtonOnBoarding.kt
@@ -17,19 +17,31 @@ class CustomButtonOnBoarding : AppCompatButton {
     private var txtColor: Int = 0
     private var txtColorPressed: Int = 0
     private var defaultBtnBackground: Drawable
+    private var pressedBtnBackground: Drawable
 
     init {
         txtColor = ContextCompat.getColor(context, R.color.btnTextColor)
         txtColorPressed = ContextCompat.getColor(context, R.color.white)
         defaultBtnBackground =
             ContextCompat.getDrawable(context, R.drawable.bg_btn_onboarding) as Drawable
+        pressedBtnBackground =
+            ContextCompat.getDrawable(context, R.drawable.bg_btn_onboarding_pressed) as Drawable
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        background = defaultBtnBackground
-        if (isPressed) setTextColor(txtColorPressed) else setTextColor(txtColor)
+//        background = if (isPressed) pressedBtnBackground else defaultBtnBackground
+//        if (isPressed) setTextColor(txtColorPressed) else setTextColor(txtColor)
+
+        if(isPressed) {
+            background = pressedBtnBackground
+            setTextColor(txtColorPressed)
+        } else {
+            background = defaultBtnBackground
+            setTextColor(txtColor)
+        }
+
         gravity = Gravity.CENTER
     }
 
