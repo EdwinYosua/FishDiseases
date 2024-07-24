e4c5498aa5baacdf9c2280194c049965e1d3cf49
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-07-24 13:16:51 +0800

    Navigation, HomeFragment
    adding a few navigation's,
    create HomeFragment

---
+++ app/src/main/java/com/edwinyosua/fishdiseasesapp/ui/home/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.edwinyosua.fishdiseasesapp.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.edwinyosua.fishdiseasesapp.base.BaseFragment
+import com.edwinyosua.fishdiseasesapp.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+    override fun getViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): FragmentHomeBinding = FragmentHomeBinding.inflate(inflater, container, false)
+
+    override fun initInsets(): Boolean = true
+
+    override fun initIntent() {}
+
+    override fun initUI() {}
+
+    override fun initAction() {}
+
+    override fun initProcess() {}
+
+    override fun initObservers() {}
+
+
+}
