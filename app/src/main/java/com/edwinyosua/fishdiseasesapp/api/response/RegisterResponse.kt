67ed527a3ded311c6999ea10bd061869d5ae20eb
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-07-29 22:37:57 +0800

    Auth Function,
    adding auth response

---
+++ app/src/main/java/com/edwinyosua/fishdiseasesapp/api/response/RegisterResponse.kt
@@ -0,0 +1,12 @@
+package com.edwinyosua.fishdiseasesapp.api.response
+
+import com.google.gson.annotations.SerializedName
+
+data class RegisterResponse(
+
+	@field:SerializedName("message")
+	val message: String? = null,
+
+	@field:SerializedName("user")
+	val user: String? = null
+)
