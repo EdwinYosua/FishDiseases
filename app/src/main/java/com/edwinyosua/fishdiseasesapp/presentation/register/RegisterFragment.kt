8b94c2940ab010c932af941118fe5d575b383285
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-08-15 15:18:40 +0800

    modify toasty and improved field check in register

--- app/src/main/java/com/edwinyosua/fishdiseasesapp/presentation/register/RegisterFragment.kt
+++ app/src/main/java/com/edwinyosua/fishdiseasesapp/presentation/register/RegisterFragment.kt
@@ -1,6 +1,7 @@
 package com.edwinyosua.fishdiseasesapp.presentation.register
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
@@ -9,6 +10,8 @@ import com.edwinyosua.fishdiseasesapp.R
 import com.edwinyosua.fishdiseasesapp.base.BaseFragment
 import com.edwinyosua.fishdiseasesapp.data.network.ApiResult
 import com.edwinyosua.fishdiseasesapp.databinding.FragmentRegisterBinding
+import com.edwinyosua.fishdiseasesapp.utils.ext.isEmailValid
+import com.edwinyosua.fishdiseasesapp.utils.ext.toastyMsg
 import es.dmoral.toasty.Toasty
 import org.koin.android.ext.android.inject
 
@@ -38,11 +41,39 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding>() {
                 val name = editFullname.text?.trim().toString()
                 val email = editEmail.text?.trim().toString()
                 val pass = editNewPass.text?.trim().toString()
+                val passConfirm = editConfirmPass.text?.trim().toString()
 
-                //adding pass confirmation check later
-                //adding empty field check later
 
-                registerViewModel.register(name, email, pass)
+
+
+                if (pass != passConfirm) toastyMsg(requireContext(), "Password Doesn't Match", 2)
+
+                if (passConfirm.isEmpty()) toastyMsg(requireContext(), "Confirmation Password is Empty !", 2)
+
+                if(pass.length < 8) toastyMsg(requireContext(), "Password is Less Than 8 Character", 2)
+
+                if (pass.isEmpty()) toastyMsg(requireContext(), "Password is Empty !", 2)
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches() && email.isNotEmpty()) toastyMsg(requireContext(), "Wrong Email Format !", 2)
+
+                if (email.isEmpty()) toastyMsg(requireContext(), "Email is Empty !", 2)
+
+                if (name.isEmpty()) toastyMsg(requireContext(), "Name is Empty !", 2)
+
+                when {
+                    email.isEmailValid() && pass.length >= 8 && passConfirm == pass -> {
+                        registerViewModel.register(name, email, pass)
+                    }
+                }
+
+
+//                if (
+//                    name.isNotEmpty() &&
+//                    (email.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) &&
+//                    (pass.isNotEmpty() && pass == passConfirm)
+//                ) {
+//                    registerViewModel.register(name, email, pass)
+//                }
             }
 
             txvLinkLogin.setOnClickListener {
