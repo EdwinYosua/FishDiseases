7bbf0a183d983208cb4bea563b8e4ef2f286b229
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-08-20 00:09:09 +0800

    home view model
    adding HomeViewModel.kt to ViewModelModule.kt for koin di

--- app/src/main/java/com/edwinyosua/fishdiseasesapp/di/modules/ViewModelModule.kt
+++ app/src/main/java/com/edwinyosua/fishdiseasesapp/di/modules/ViewModelModule.kt
@@ -1,5 +1,6 @@
 package com.edwinyosua.fishdiseasesapp.di.modules
 
+import com.edwinyosua.fishdiseasesapp.presentation.home.HomeViewModel
 import com.edwinyosua.fishdiseasesapp.presentation.login.LoginViewModel
 import com.edwinyosua.fishdiseasesapp.presentation.register.RegisterViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -8,4 +9,5 @@ import org.koin.dsl.module
 val viewModelModule = module {
     viewModel { LoginViewModel(get()) }
     viewModel { RegisterViewModel(get()) }
+    viewModel { HomeViewModel(get()) }
 }
--- app/src/main/java/com/edwinyosua/fishdiseasesapp/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/edwinyosua/fishdiseasesapp/presentation/home/HomeViewModel.kt
@@ -1,9 +1,28 @@
 package com.edwinyosua.fishdiseasesapp.presentation.home
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.edwinyosua.fishdiseasesapp.domain.auth.IAuthRepository
+import androidx.lifecycle.viewModelScope
+import com.edwinyosua.fishdiseasesapp.data.network.ApiResult
+import com.edwinyosua.fishdiseasesapp.domain.prediction.ModelUseCase
+import com.edwinyosua.fishdiseasesapp.domain.prediction.entities.Prediction
+import kotlinx.coroutines.launch
+import java.io.File
 
-class HomeViewModel(private val authRepo: IAuthRepository) : ViewModel() {
+class HomeViewModel(private val modelRepository: ModelUseCase) : ViewModel() {
+
+    private val _modelResult = MutableLiveData<ApiResult<Prediction>>()
+    val modelResult: LiveData<ApiResult<Prediction>> by lazy { _modelResult }
 
 
+    fun analyzeImage(img: File) {
+        viewModelScope.launch {
+            modelRepository.predict(img).collect {
+                _modelResult.value = it
+                Log.d("HomeViewModel", "analyzeImg")
+            }
+        }
+    }
 }
